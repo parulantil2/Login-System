9e09c2b39702df8702d22b86feb64428c0a351e2
parulantil2 <antilparul06@gmail.com>
2024-08-07 15:00:11 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
-# from .views import UserListView
-from .views import user_create_view, user_detail_view
+from .views import UserDetailView, UserListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('users/', user_create_view, name='user-list-create'),
-    path('users/<int:pk>/', user_detail_view, name='user-details'),
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-details'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+
 ]
