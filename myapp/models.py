3db5e5e403e3d166b0ef53427976c1a5e8d2d59c
parulantil2 <antilparul06@gmail.com>
2024-08-09 10:25:32 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-# # Create your models here.
 
 
 class Registration(models.Model):
     username = models.CharField(max_length=50),
     password = models.CharField(max_length=20)
+    email = models.EmailField(default='default@example.com', help_text='Enter a valid email address.')
 
 
 def __str__(self):
     return self.name
+
+
